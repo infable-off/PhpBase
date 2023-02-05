5aa26862398d5bc238eb7f41d5e4fbe070efe50e
Vladislav Gusev <73646096+infable-off@users.noreply.github.com>
2023-02-05 19:07:11 +0300

    Update task1.php

--- HW3/task1.php
+++ HW3/task1.php
@@ -1,1 +1,14 @@
+<?php
+$arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+$arr2 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11];
 
+$result = [];
+
+if (count($arr1) == count($arr2)) {
+    for ($i = 0; $i < count($arr1); $i++) {
+        $result[$i] = $arr1[$i] * $arr2[$i];
+    }
+    print_r($result);
+} else {
+    echo "Error...";
+}
